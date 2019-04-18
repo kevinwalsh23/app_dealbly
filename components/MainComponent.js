6d9ef88ddcbb84e3b988e2fa97fb1479e84020d7
kevinwalsh23 <kevinwalsh23@gmail.com>
2019-04-18 02:07:49 -0400

    dealbly framework commit

--- components/MainComponent.js
+++ components/MainComponent.js
@@ -2,31 +2,134 @@ import React, { Component } from 'react';
 import Menu from './MenuComponent';
 import { DISHES } from '../shared/dishes';
 import Dishdetail from './DishDetailComponent';
-import { View } from 'react-native';
+import { View, Platform, Text, ScrollView, Image, StyleSheet } from 'react-native';
+import { createStackNavigator, createAppContainer, createDrawerNavigator, DrawerItems, SafeAreaView } from 'react-navigation';
+import Home from './HomeComponent';
+import { Icon } from 'react-native-elements';
+
+const HomeNavigator = createStackNavigator({
+    Home: { screen: Home }
+  }, {
+    defaultNavigationOptions: ({ navigation }) => ({
+      headerStyle: {
+          backgroundColor: "green"
+      },
+      headerTitleStyle: {
+          color: "#fff"            
+      },
+      headerTintColor: "#fff",
+      headerLeft: <Icon name='menu' size={24} color='white' onPress={() => navigation.toggleDrawer()}/>
+    })
+});
+
+const MenuNavigator = createStackNavigator({
+    Menu: { screen: Menu },
+    Dishdetail: { screen: Dishdetail }
+},
+{
+    initialRouteName: 'Menu',
+    defaultNavigationOptions: ({ navigation }) => ({
+        headerTintColor: '#fff',
+        headerStyle: { backgroundColor: 'green' },
+        headerTitleStyle: { color: '#fff'},
+        headerLeft: <Icon name='menu' size={24} color='white' onPress={() => navigation.toggleDrawer()}/>
+    })
+}
+);
+
+const CustomDrawerContentComponent = (props) => (
+    <ScrollView>
+      <SafeAreaView style={styles.container} forceInset={{ top: 'always', horizontal: 'never' }}>
+        <View style={styles.drawerHeader}>
+          <View style={{flex: 2}}>
+            <Text style={styles.drawerHeaderText}>dealbly</Text>
+          </View>
+        </View>
+        <DrawerItems {...props} />
+      </SafeAreaView>
+    </ScrollView>
+  );
+const MainNavigator = createDrawerNavigator({
+    Home: {
+        screen: HomeNavigator,
+        defaultNavigationOptions: {
+          title: 'Home',
+          drawerLabel: 'Home',
+          drawerIcon: ({ tintColor, focused }) => (
+              <Icon name='home' type='font-awesome' size={24} color={tintColor}/>
+          )
+        }
+      },
+    Menu: 
+      { screen: MenuNavigator,
+        defaultNavigationOptions: {
+          title: 'Menu',
+          drawerLabel: 'Menu',
+          drawerIcon: ({ tintColor, focused }) => (
+            <Icon
+              name='list'
+              type='font-awesome'            
+              size={24}
+              color={tintColor}
+            />
+          ),
+          
+        }, 
+      }
+}, {
+  drawerBackgroundColor: '#F5F5F5',
+  contentComponent: CustomDrawerContentComponent
+});
+
+const App = createAppContainer(MainNavigator);
 
 class Main extends Component {
     constructor(props) {
-      super(props);
-      this.state = {
-        dishes: DISHES,
-        selectedDish: null
-      };
+        super(props);
+        this.state = {
+            dishes: DISHES,
+            selectedDish: null
+        }
     }
+
     onDishSelect(dishId) {
-        this.setState({selectedDish: dishId })
+        this.setState({selectedDish: dishId});
     }
-
-
-
 
     render() {
         return (
-            <View style={{flex:1}}>
-                <Menu dishes={this.state.dishes} onPress={(dishId) => this.onDishSelect(dishId)} />
-                <Dishdetail dish={this.state.dishes.filter((dish) => dish.id === this.state.selectedDish)[0]} />
+            <View style={{flex:1, paddingTop: Platform.OS === 'ios' ? 0 : Expo.Constants.statusBarHeight }}>
+                <App />
             </View>
-        )
+        );
     }
 }
 
+const styles = StyleSheet.create({
+    container: {
+      flex: 1,
+    },
+    drawerHeader: {
+      backgroundColor: 'green',
+      height: 140,
+      alignItems: 'center',
+      justifyContent: 'center',
+      flex: 1,
+      flexDirection: 'row'
+    },
+    drawerHeaderText: {
+      color: 'white',
+      fontSize: 24,
+      fontWeight: 'bold',
+      alignItems: 'center',
+      justifyContent: 'center',
+      textAlign: 'center'
+      
+    },
+    drawerImage: {
+      margin: 10,
+      width: 80,
+      height: 60
+    }
+  });
 export default Main;
